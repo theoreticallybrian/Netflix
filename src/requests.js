4ded3d88177c0fce424b9b9501893b17861e81ae
theoreticallybrian <57189551+theoreticallybrian@users.noreply.github.com>
2022-11-27 11:54:47 +0300

    new changes

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+const API_KEY = "fe4628bc997664ae5a4dacfb4f8c5d21";
+
+const request = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+
+};
+
+
+export default request;
